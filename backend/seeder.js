5af6661fe1f4ec9c9ec0461a2f467e31c3353240
Tushank <tushankr0020@gmail.com>
2026-06-07 18:01:30 +0530

    mongo temp

--- backend/seeder.js
+++ backend/seeder.js
@@ -789,7 +789,8 @@ const generateFakeReviews = (numReviews, avgRating, createdUsers) => {
 
 const seedDatabase = async () => {
   try {
-    await mongoose.connect(process.env.MONGO_URI);
+    await mongoose.connect("mongodb+srv://tushankr0020:Mongo123@cluster0.oehhe.mongodb.net/ecommerce?appName=Cluster0");
+
     console.log("MongoDB Connected");
 
     // Clear existing data
